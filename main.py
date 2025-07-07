f787f628b2479681c0ee3313b6d047f7e38cf6ab
SPAMIK356 <maximbondarenko20@gmail.com>
2025-07-07 18:19:59 +0300

    Setted up pygame

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pygame
+
+
+
+FPS = 60
+WIDTH = 1280
+HEIGHT = 720
+CELL_SIZE = 5
+C_EMPTY = (100,100,100)
+C_TAKEN = (0,0,0)
+
+#Pygame setup
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+a = np.random.randint(0,2, (100,100))
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill('white')
+
+    pygame.display.flip()
+    clock.tick(FPS)
