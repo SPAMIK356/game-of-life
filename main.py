2c34e86154b1f43800febb2f8199d59f7e9ee915
SPAMIK356 <maximbondarenko20@gmail.com>
2025-07-08 14:51:14 +0300

    Implemented rules

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ def draw_grid(grid: np.ndarray, cell_size: float, colors: np.ndarray, surface: p
     x_max = grid.shape[0]
     y_max = grid.shape[1]
 
-    for x in range(x_max):
-        for y in range(y_max):
+    for y in range(y_max):
+        for x in range(x_max):
             pygame.draw.rect(surface,colors[x,y], Rect(x*cell_size,y*cell_size, cell_size,cell_size)) 
 
 
 def get_neighbours(grid: np.array) -> np.array:
     neigbours = np.zeros(shape=grid.shape)
-    transformations = np.array([-1,0,1])
-    for x in transformations:
-        for y in transformations:
+    
+    for x in range(-1,2):
+        for y in range(-1,2):
             if x == 0 and y == 0:
                 continue
             neigbours += np.roll(grid, (x,y),(0,1))
     return neigbours
             
 
-FPS = 60
+FPS = 1
 WIDTH = 1280
 HEIGHT = 720
-CELL_SIZE = 5.0
-cell_colors = np.array([(200,200,200),(0,0,0)])
+CELL_SIZE = 50.0
+cell_colors = np.array([(250,0,0),(0,250,0)])
 
 #Pygame setup
 pygame.init()
@@ -34,6 +34,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+grid = np.random.randint(0,2, (3,3))
 
 while running:
     for event in pygame.event.get():
@@ -42,9 +43,20 @@ while running:
 
     screen.fill('white')
 
-    a = np.random.randint(0,2, (100,100))
-    colors = cell_colors[a]
-    draw_grid(a,CELL_SIZE,colors,screen)
+    colors = cell_colors[grid]
+
+    draw_grid(grid,CELL_SIZE,colors,screen)
 
     pygame.display.flip()
+
+    neigbours = get_neighbours(grid)
+
+    empty = grid == 0
+    taken = grid == 1
+    
+    grid[(neigbours<2) & taken] = 0
+    grid[(neigbours > 3) & taken] = 0
+    grid[np.logical_or(neigbours == 2, neigbours == 3) &  taken] = 1
+    grid[(neigbours == 3) & empty] = 1
+
     clock.tick(FPS)
